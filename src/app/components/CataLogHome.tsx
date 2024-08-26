2f4cb083f2f7223abf2652695364d172dc2c725a
Prajwal <prajwal.ratnaparkhe18@gmail.com>
2024-08-26 21:30:39 +0530

    click to top and datasets add

--- src/app/components/CataLogHome.tsx
+++ src/app/components/CataLogHome.tsx
@@ -18,6 +18,7 @@ import Publications from "./Publications";
 import AITools from "./AITools";
 import styles from "./CataLogHome.module.css";
 import useSearchStore from "./Searchstore";
+import ScrollToTop from "react-scroll-to-top";
 
 const CataLogHome = ({ datasets, models, tools, publications }) => {
   const query = useSearchStore((state) => state.query);
@@ -56,56 +57,85 @@ const CataLogHome = ({ datasets, models, tools, publications }) => {
   const filteredTools = filterData(tools);
   const filteredPublications = filterData(publications);
 
+  const getDatasetsResultCount = (filteredData: any[]) => filteredData.length;
+  const getModelsResultCount = (filteredData: any[]) => filteredData.length;
+  const getToolsResultCount = (filteredData: any[]) => filteredData.length;
+  const getPublicationsResultCount = (filteredData: any[]) =>
+    filteredData.length;
+
   return (
-    <FlexGrid className={styles.flexGrid}>
-      <Row style={{ flex: 1 }}>
-        <Column sm={3} md={3} lg={3}>
-          <Tabs>
-            <TabList aria-label="List of tabs">
-              <Tab className="tab-item">Data Sources</Tab>
-              <Tab className="tab-item">AI Models</Tab>
-              <Tab className="tab-item">Publications</Tab>
-              <Tab className="tab-item">AI Tools</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                {filteredDatasets.length === 0 ? (
-                  <p>No results found</p>
-                ) : (
-                  <DataSources datasets={filteredDatasets} />
-                )}
-              </TabPanel>
-              <TabPanel>
-                {filteredModels.length === 0 ? (
-                  <p>No results found</p>
-                ) : (
-                  <AIModels models={filteredModels} />
-                )}
-              </TabPanel>
-              <TabPanel>
-                {filteredPublications.length === 0 ? (
-                  <p>No results found</p>
-                ) : (
-                  <Publications publications={filteredPublications} />
-                )}
-              </TabPanel>
-              <TabPanel>
-                {filteredTools.length === 0 ? (
-                  <p>No results found</p>
-                ) : (
-                  <AITools tools={filteredTools} />
-                )}
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </Column>
-      </Row>
-      <Row style={{ flex: 1 }}>
-        <Column sm={9} md={9} lg={9} className="content-column">
-          {/* This column will hold the selected TabPanel */}
-        </Column>
-      </Row>
-    </FlexGrid>
+    <div>
+      <FlexGrid className={styles.flexGrid}>
+        <Row style={{ flex: 1 }}>
+          <Column sm={3} md={3} lg={3}>
+            <Tabs>
+              <TabList aria-label="List of tabs">
+                <Tab className="tab-item">Data Sources</Tab>
+                <Tab className="tab-item">AI Models</Tab>
+                <Tab className="tab-item">Publications</Tab>
+                <Tab className="tab-item">AI Tools</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <p>
+                    Total Results Found:{" "}
+                    {getDatasetsResultCount(filteredDatasets)}
+                  </p>
+
+                  {filteredDatasets.length === 0 ? (
+                    <p>No results found</p>
+                  ) : (
+                    <DataSources datasets={filteredDatasets} />
+                  )}
+                </TabPanel>
+                <TabPanel>
+                  <p>
+                    Total Results Found:{" "}
+                    {getModelsResultCount(filteredModels)}
+                  </p>
+                  {filteredModels.length === 0 ? (
+                    <p>No results found</p>
+                  ) : (
+                    <AIModels models={filteredModels} />
+                  )}
+                </TabPanel>
+                <TabPanel>
+                  <p>
+                    Total Results Found:{" "}
+                    {getPublicationsResultCount(filteredPublications)}
+                  </p>
+                  {filteredPublications.length === 0 ? (
+                    <p>No results found</p>
+                  ) : (
+                    <Publications publications={filteredPublications} />
+                  )}
+                </TabPanel>
+                <TabPanel>
+                  <p>
+                    Total Results Found: {getToolsResultCount(filteredTools)}
+                  </p>
+                  {filteredTools.length === 0 ? (
+                    <p>No results found</p>
+                  ) : (
+                    <AITools tools={filteredTools} />
+                  )}
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Column>
+        </Row>
+        <Row style={{ flex: 1 }}>
+          <Column sm={9} md={9} lg={9} className="content-column">
+            {/* This column will hold the selected TabPanel */}
+          </Column>
+        </Row>
+      </FlexGrid>
+
+      <ScrollToTop
+        smooth
+        component={<button className={styles.scrollTopButton}>↑</button>}
+      />
+    </div>
   );
 };
 
