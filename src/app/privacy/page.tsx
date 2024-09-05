a5df42b5099df612b0f0e470ca3915839d0400b1
Anindita Das <dasanuiit@gmail.com>
2024-09-05 10:47:07 -0400

    Fixed the icon

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 import React from 'react';
 import Link from "next/link";
-import { WrenchScrewdriverIcon } from '@heroicons/react/24/outline';
+import { Construction } from  '@carbon/icons-react';
 
 export default function Page() {
   return (
       <main className="flex h-full flex-col items-center justify-center gap-2">
           <h2 className="text-xl font-semibold content-column">Privacy Policy</h2>
-          <WrenchScrewdriverIcon className="w-10 text-gray-400" />
+           <Construction className="w-screen h-16 text-orange-600" />
           <div className="content-text">
               <p>Page under construction.</p>
           </div>
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 import React from 'react';
 import Link from "next/link";
-import { WrenchScrewdriverIcon } from '@heroicons/react/24/outline';
+import { Construction } from  '@carbon/icons-react';
 
 export default function Page() {
   return (
       <main className="flex h-full flex-col items-center justify-center gap-2">
           <h2 className="text-xl font-semibold content-column">Terms of Service</h2>
-          <WrenchScrewdriverIcon className="w-10 text-gray-400" />
-          <div className="content-text">
+          <Construction className="w-screen h-16 text-orange-600" />
+          <div className="content-text" >
               <p>Page under construction.</p>
           </div>
           <Link href="/" className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400">
