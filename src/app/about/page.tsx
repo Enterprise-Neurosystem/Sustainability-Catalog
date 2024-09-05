5b9ec57477469eeff33966c4fffe04f8c5dff928
Anindita Das <dasanuiit@gmail.com>
2024-09-05 02:27:20 -0400

    Updated the title, about, privacy and terms of service page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
           <h2 className="text-xl font-semibold content-column">About Us</h2>
           <div className="content-text">
               <p>
-                  AI Sustainability Hub serves as a knowledge catalog with the theme AI for climate and
+                  AI Application Hub serves as a knowledge catalog with the theme AI for climate and
                   sustainability and is built to bring the Subject matter experts in this field together in one
                   place. The site has been categorized based on our community input into Data Sources, AI Models,
                   Publications and AI Tools.  The site also provides a search capability for each of the sections.
@@ -34,6 +34,14 @@ export default function Page() {
                   <a href= "mailto: sheeba@us.ibm.com" className="text-blue-600 hover:underline">sheeba@us.ibm.com</a>, {' '}
                   <a href= "mailto: abhay.ratnaparkhi1@ibm.com" className="text-blue-600 hover:underline">abhay.ratnaparkhi1@ibm.com</a>, {' '}
                   <a href= "mailto: anindita.das@ibm.com" className="text-blue-600 hover:underline">anindita.das@ibm.com</a>.
+              </p>
+              <br />
+              <p>
+                  <strong>Acknowledgement</strong>: All of this started with ENG CTO, Dinesh Verma’s vision for a comprehensive Data
+                  Catalog for Enterprise Neurosystems and evolved into current form with dedicated work from the Hub team (Sheeba
+                  Prakash, Abhay Ratnaparkhi and Anindita Das). Lot of volunteer hours and multiple iterations have gone into this to
+                  make our AI Application Hub a reality. The team is grateful for the opportunity and support from ENG (Dinesh Verma,
+                  Bill Wright, John Overton & others) and we look forward to more collaborations.
               </p>
           </div>
           <Link href="/" className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400">
