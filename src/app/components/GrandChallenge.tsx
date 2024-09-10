f3ace43309cffa54e6efcf95eb14210438a8c7d7
Anindita Das <dasanuiit@gmail.com>
2024-09-10 11:22:42 -0400

    Added new tabs and updated copyright

---
+++ src/app/components/GrandChallenge.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+import { Construction } from  '@carbon/icons-react';
+
+const GrandChallenge: React.FC = () => {
+  return (
+    <main className="flex h-full flex-col items-center justify-center gap-2">
+          <Construction className="w-screen h-16 text-orange-600" />
+          <div className="content-text" >
+              <p>Page under construction.</p>
+          </div>
+
+      </main>
+  );
+};
+
+export default GrandChallenge;
