ac355a6340742c759680f4f799c8c97aaf97196a
Sheeba Prakash <94703633+sheebaprakash@users.noreply.github.com>
2024-09-18 21:03:59 -0400

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,12 +10,11 @@ export default function Page() {
           <h2 className="text-xl font-semibold content-column">About Us</h2>
           <div className="content-text">
               <p>
-                  AI Application Hub serves as a knowledge catalog with the theme AI for climate action and is built to bring the Subject matter experts in this field together in one
-                  place. This is place where experts can come to gather information as well as contribute to the hub based on their expertise on climate action and AI. The site has been categorized based on our community input into Data Sources, AI Models,
-                  Publications and AI Tools.  The site also provides a search capability for each of the sections.
+                  AI Application Hub serves as a knowledge catalog with the theme "AI for climate action" and is built to bring Subject matter experts in this field in one
+                  place. This is the place where climate and AI experts come together to gather information as well as contribute (giveback) to the hub. 
               </p>
               <p>
-                  To add your content into this website submit a PR with specifying the following information to
+                  You can contribute to the contents of this website by submitting a Pull Request (PR) specifying the following information to
                   our AI Sustainability Hub Git repo in {' '}
                   <a href="https://github.com/Enterprise-Neurosystem/Sustainability-Catalog" className="text-blue-600 hover:underline">ENG Git Hub</a>.
                   <br />
@@ -28,9 +27,9 @@ export default function Page() {
                   </ul>
               <br />
                   Alternatively, send an email with your contributions to {' '}
-                  <a href= "mailto: support1" className="text-blue-600 hover:underline">sheeba@us.ibm.com</a>, {' '}
-                  <a href= "mailto: support2" className="text-blue-600 hover:underline">abhay.ratnaparkhi1@ibm.com</a>, {' '}
-                  <a href= "mailto: support3" className="text-blue-600 hover:underline">anindita.das@ibm.com</a>.
+                  <a href= "mailto: sheeba@us.ibm.com" className="text-blue-600 hover:underline">en_support1</a>, {' '}
+                  <a href= "mailto: abhay.ratnaparkhi1@ibm.com" className="text-blue-600 hover:underline">en_support2</a>, {' '}
+                  <a href= "mailto: anindita.das@ibm.com" className="text-blue-600 hover:underline">en_support3</a>.
               <br />
               <br />
                  
